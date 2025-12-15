62786909675d18acab1fb723b48ff2aa0056ab7a
Prajakta Pathak <prajakta.coding.email@gmail.com>
2025-12-15 10:36:35 -0500

    Added comment.

--- YummyRecipes/YummyRecipesApp.swift
+++ YummyRecipes/YummyRecipesApp.swift
@@ -4,6 +4,7 @@ import SwiftUI
 struct YummyRecipesApp: App {
     var body: some Scene {
         WindowGroup {
+            // This is my Yummy Recipes simple App.
             RecipesListView(
                 recipesURL: URL(string: "https://d3jbb8n5wk0qxi.cloudfront.net/recipes.json")!,
 //                recipesURL: URL(string: "https://d3jbb8n5wk0qxi.cloudfront.net/recipes-malformed.json")!,
